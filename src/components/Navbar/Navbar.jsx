2e4cf7447dec3903f8cbc3bfba5764f4f875ec74
Aiman SIngh <145761115+Aimans30@users.noreply.github.com>
2024-02-07 16:26:54 +0530

    Add files via upload

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,57 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { SearchQueryContext } from "../../App";
+import "./Navbar.css";
+
+function Navbar() {
+  const { searchQuery, setSearchQuery } = useContext(SearchQueryContext);
+  const [searchInput, setSearchInput] = useState("");
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearchQuery(searchInput);
+    setSearchInput("");
+    if (location.pathname !== "/") {
+      navigate("/");
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setSearchInput(e.target.value);
+  };
+
+  useEffect(() => {
+    setSearchInput(searchQuery);
+  }, [searchQuery]);
+
+  return (
+    <nav className="navbar-container">
+      <Link to="/" className="navbar-logo" onClick={() => setSearchQuery("")}>
+        <img src="./logo.svg" alt="" />
+        <span>Books</span>
+      </Link>
+      <form className="navbar-search-container" onSubmit={handleSearch}>
+        <input
+          className="navbar-search-input"
+          type="text"
+          placeholder="Search Books"
+          value={searchInput}
+          onChange={handleInputChange}
+        />
+        <button className="navbar-search-btn" type="submit">
+          <img src="./icons/search.svg" alt="" />
+        </button>
+      </form>
+
+      <div className="navbar-cta-register">
+        <Link to="/register" className="navbar-login-link">
+          Login
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
